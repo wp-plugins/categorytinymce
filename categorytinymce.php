64b06fcf4bbfd00b7b1cf979ae9ad421e2590639
ypraise <ypraise@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-07 13:01:38 +0000

    forgot some changes to version
    git-svn-id: https://plugins.svn.wordpress.org/categorytinymce/trunk@486148 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- categorytinymce.php
+++ categorytinymce.php
@@ -3,7 +3,7 @@
 Plugin Name: CategoryTinymce
 Plugin URI: http://ypraise.com/2012/01/wordpress-plugin-categorytinymce/
 Description: Adds a tinymce enable box to the category descriptions page.
-Version: 1.0
+Version: 1.1
 Author: Kevin Heath
 Author URI: http://ypraise.com
 License: GPL
